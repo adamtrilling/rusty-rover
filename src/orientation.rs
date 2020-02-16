feb64f092afc58219d56cede45defc8fd245e742
Adam Trilling <adamtrilling@gmail.com>
2020-02-15 19:51:40 -0500

    implement rover movement

--- src/orientation.rs
+++ src/orientation.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Orientation {
     North,
     East,
@@ -23,5 +23,44 @@ impl Orientation {
             Orientation::South => Orientation::West,
             Orientation::East => Orientation::South,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn north_turn_left() {
+        assert_eq!(Orientation::North.turn_left(), Orientation::West)
+    }
+    #[test]
+    fn west_turn_left() {
+        assert_eq!(Orientation::West.turn_left(), Orientation::South)
+    }
+    #[test]
+    fn south_turn_left() {
+        assert_eq!(Orientation::South.turn_left(), Orientation::East)
+    }
+    #[test]
+    fn east_turn_left() {
+        assert_eq!(Orientation::East.turn_left(), Orientation::North)
+    }
+
+    #[test]
+    fn north_turn_right() {
+        assert_eq!(Orientation::North.turn_right(), Orientation::East)
+    }
+    #[test]
+    fn west_turn_right() {
+        assert_eq!(Orientation::West.turn_right(), Orientation::North)
+    }
+    #[test]
+    fn south_turn_right() {
+        assert_eq!(Orientation::South.turn_right(), Orientation::West)
+    }
+    #[test]
+    fn east_turn_right() {
+        assert_eq!(Orientation::East.turn_right(), Orientation::South)
     }
 }
--- src/rover.rs
+++ src/rover.rs
@@ -1,13 +1,13 @@
 use super::orientation::Orientation;
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 enum Instruction {
     Move,
     Left,
     Right,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Rover {
     x: u32,
     y: u32,
@@ -15,41 +15,115 @@ pub struct Rover {
     instructions: Vec<Instruction>,
 }
 
+impl Rover {
+    pub fn turn_left(&self) -> Self {
+        Rover {
+            orientation: self.orientation.turn_left(),
+            ..self.clone()
+        }
+    }
+
+    pub fn turn_right(&self) -> Self {
+        Rover {
+            orientation: self.orientation.turn_right(),
+            ..self.clone()
+        }
+    }
+
+    pub fn r#move(&self) -> Self {
+        let (new_x, new_y) = match self.orientation {
+            Orientation::North => (self.x, self.y + 1),
+            Orientation::South => (self.x, self.y - 1),
+            Orientation::West => (self.x - 1, self.y),
+            Orientation::East => (self.x + 1, self.y),
+        };
+
+        Rover {
+            x: new_x,
+            y: new_y,
+            ..self.clone()
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn north_turn_left() {
-        assert_eq!(Orientation::North.turn_left(), Orientation::West)
-    }
-    #[test]
-    fn west_turn_left() {
-        assert_eq!(Orientation::West.turn_left(), Orientation::South)
-    }
-    #[test]
-    fn south_turn_left() {
-        assert_eq!(Orientation::South.turn_left(), Orientation::East)
-    }
-    #[test]
-    fn east_turn_left() {
-        assert_eq!(Orientation::East.turn_left(), Orientation::North)
+    fn test_rover() -> Rover {
+        Rover {
+            x: 1,
+            y: 2,
+            orientation: Orientation::North,
+            instructions: vec![
+                Instruction::Move,
+                Instruction::Move,
+                Instruction::Right,
+                Instruction::Move,
+                Instruction::Move,
+                Instruction::Right,
+                Instruction::Move,
+                Instruction::Right,
+                Instruction::Right,
+                Instruction::Move,
+            ],
+        }
     }
 
     #[test]
-    fn north_turn_right() {
-        assert_eq!(Orientation::North.turn_right(), Orientation::East)
+    fn doing_turn_left_turns_left() {
+        let rover = test_rover();
+        let new_rover = rover.turn_left();
+
+        assert_eq!(new_rover.orientation, Orientation::West)
     }
+
     #[test]
-    fn west_turn_right() {
-        assert_eq!(Orientation::West.turn_right(), Orientation::North)
+    fn doing_turn_right_turns_right() {
+        let rover = test_rover();
+        let new_rover = rover.turn_right();
+
+        assert_eq!(new_rover.orientation, Orientation::East)
     }
+
     #[test]
-    fn south_turn_right() {
-        assert_eq!(Orientation::South.turn_right(), Orientation::West)
+    fn moving_north_increases_y() {
+        let rover = test_rover();
+        let new_rover = rover.r#move();
+
+        assert_eq!(new_rover.y, 3)
     }
+
     #[test]
-    fn east_turn_right() {
-        assert_eq!(Orientation::East.turn_right(), Orientation::South)
+    fn moving_south_decreases_y() {
+        let rover = Rover {
+            orientation: Orientation::South,
+            ..test_rover()
+        };
+        let new_rover = rover.r#move();
+
+        assert_eq!(new_rover.y, 1)
+    }
+
+    #[test]
+    fn moving_west_decreases_x() {
+        let rover = Rover {
+            orientation: Orientation::West,
+            ..test_rover()
+        };
+        let new_rover = rover.r#move();
+
+        assert_eq!(new_rover.x, 0)
+    }
+
+    #[test]
+    fn moving_east_decreases_y() {
+        let rover = Rover {
+            orientation: Orientation::East,
+            ..test_rover()
+        };
+        let new_rover = rover.r#move();
+
+        assert_eq!(new_rover.x, 2)
     }
 }
